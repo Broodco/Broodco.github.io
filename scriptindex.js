6e5326aadc912fc96d2670b2da7b8745727a15b8
Broodco <broodcoorens.maxime@gmail.com>
2019-01-23 15:00:45 +0100

    index page added

---
+++ scriptindex.js
@@ -0,0 +1,20 @@
+/* This script is used to create a drop down button in the header part of the page */
+
+function showdropdowna(){
+    document.getElementById("myDropdown-exos").classList.toggle("show");
+}
+function showdropdownb(){
+    document.getElementById("myDropdown-contact").classList.toggle("show");
+}
+window.onclick = function(event){
+    if (!event.target.matches('.dropdown')){
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i=0 ; i < dropdowns.length ; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
